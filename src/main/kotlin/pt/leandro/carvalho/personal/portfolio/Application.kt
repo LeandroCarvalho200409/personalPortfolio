6c1ab9056d8c8e303bd372ece2a66eaa842aedb9
Leandro200409 <78601676+Leandro200409@users.noreply.github.com>
2024-05-08 23:41:10 +0200

    Initial commit

---
+++ src/main/kotlin/pt/leandro/carvalho/personal/portfolio/Application.kt
@@ -0,0 +1,11 @@
+package pt.leandro.carvalho.personal.portfolio
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+	runApplication<Application>(*args)
+}
